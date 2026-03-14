a3202e3f35dce656abf23779a3373277bc8024e1
Tim Schupp <tim@timschupp.de>
2026-03-14 15:56:47 +0100

    Force use of local schemas via buildInputs override

--- flake.nix
+++ flake.nix
@@ -82,7 +82,10 @@
       # Inject our updated subprojects
       # We put my-blueprint FIRST in nativeBuildInputs so it supersedes the older version from nixpkgs
       nativeBuildInputs = [ my-blueprint ] ++ (old.nativeBuildInputs or []) ++ [ pkgs.git ];
-      buildInputs = (old.buildInputs or []);
+      
+      # Overriding the input via `pkg.override` wasn't enough because the buildInputs list overrides it
+      # again down the line. We must force it here via buildInputs.
+      buildInputs = (pkgs.lib.remove pkgs.gsettings-desktop-schemas (old.buildInputs or [])) ++ [ my-schemas ];
 
       postInstall = (old.postInstall or "") + ''
         # We need to make sure the app finds the schemas at runtime.
